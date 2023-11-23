0cc10ddc7ddfb8ed42b6e7c072ef7024f363b6ba
Yara Xjavier <122989946+VegasVvegas@users.noreply.github.com>
2023-11-22 23:21:24 -0300

    Mudanças da classe usuario

--- src/entities/Usuario.java
+++ src/entities/Usuario.java
@@ -1,16 +1,70 @@
 package entities;
+import java.util.Date;
 
 public class Usuario extends Pessoa{
-	private Arraylist playlistsSalvas;
-	private Arraylist albunsFavoritados;
+	private Playlist[] playlistsSalvas;
+	private Album[] albunsFavoritados;
+	private int nPlaylists = 0;
+	private int nAlbuns = 0;
+	
+public Usuario(String nome,
+		   	   Date dataNascimento,
+		       String email,
+		       String senha
+		       ) {
+	super(nome, dataNascimento, email, senha);
+	playlistsSalvas = new Playlist[50];
+	albunsFavoritados = new Album[50];
+		
+	}
+		
+public Playlist[] getPlaylistsSalvas() {
+	return playlistsSalvas;
+}
+
+public int getPlaylists() {
+	return nPlaylists;
+}
+
+public void setPlaylistsSalvas(Playlist[] playlistsSalvas ) {
+	this.playlistsSalvas = playlistsSalvas;
+}
+
+public Album[] getAlbunsFavoritados() {
+	return albunsFavoritados;
+}
+
+public int getalbuns() {
+	return nAlbuns;
+}
+
+public void setAlbunsFavoritados(Album[] albunsFavoritados) {
+	this.albunsFavoritados = albunsFavoritados;
+}
+
+public void criarPlaylist(String nomePlaylist, String tituloMusica) {
+    Playlist novaPlaylist = new Playlist(nomePlaylist);
+    novaPlaylist.adicionarMusica(tituloMusica);
+    playlistsSalvas[nPlaylists++] = novaPlaylist;
+}
 
 	
-	public void criarPlaylist(String nomePlaylist, String tituloMusica) {
-	}
-	
-	public void deletarPlaylist(String nomePlaylist) {
-	}
+public void deletarPlaylist(String nomePlaylist) {
+    for (int i = 0; i < nPlaylists; i++) {
+        if (playlistsSalvas[i].getNome().equals(nomePlaylist)) {
+            playlistsSalvas[i] = null;
+            for (int j = i; j < nPlaylists - 1; j++) {
+                playlistsSalvas[j] = playlistsSalvas[j + 1];
+            }
+            playlistsSalvas[nPlaylists - 1] = null;
+            nPlaylists--;
+            break;
+        }
+    }
+}
+
 			
-	public void reproduzirMusica(String tituloMusica) {
+	public boolean reproduzirMusica(String tituloMusica) {
+		return true;
 	}
 }
