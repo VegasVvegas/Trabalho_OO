1bc72719ca1d3a83241b860966ace5daaa688a40
Matheus-0217 <149620662+Matheus-0217@users.noreply.github.com>
2023-11-22 21:45:21 -0300

    Update Spotify.java

--- src/entities/Spotify.java
+++ src/entities/Spotify.java
@@ -1,63 +1,3 @@
-package entities;
-
-import java.util.ArrayList;
-
-public class Spotify {
-	private  ArrayList<Usuario> usuarios;
-	private  ArrayList<Artista> artistas;
-	private  ArrayList<Musica> musicas;
-	private  ArrayList<Playlist> playlists;
-	private  ArrayList<Album> albuns;
-	
-	public Spotify (
-			ArrayList<Usuario> usuarios,
-			ArrayList<Artista> artistas,
-			ArrayList<Musica> musicas,
-			ArrayList<Playlist> playlists,
-			ArrayList<Album> albuns){
-		this.usuarios = new ArrayList<>(); 
-	}
-	
-	public void cadastrarPessoa(String nome,String dataNascimento, String email, String senha) {	
-	}
-	public void deletarPessoa(String nome) {	
-	}
-	public void editarPessoa(String nome,String dataNascimento, String email, String senha) {	
-	}
-	public ArrayList<Usuario> getUsuario(){
-		return usuarios;
-	}
-	public ArrayList<Artista> getArtista(){
-		return artistas;
-	}
-	public ArrayList<Musica> getMusica(){
-		return musicas;
-	}
-	public ArrayList<Playlist> getPlaylist(){
-		return playlists;
-	}
-	public ArrayList<Album> getAlbum(){
-		return albuns;
-	}
-	public ArrayList<Musica> pesquisaMusica() {
-        return musicas;
-    }	
-	public ArrayList<Artista> pesquisaArtistas(){
-		return artistas;
-	}
-	public ArrayList<Album> pesquisaAlbuns(){
-		return albuns;
-	}
-	public ArrayList<Playlist> pesquisaPlaylists(){
-		return playlists;
-	}
-}
-
-
-
-
-
-
 package Spotify;
 
 public class Spotify {
@@ -256,7 +196,7 @@ public class Spotify {
 
 
 {
-	public void preencherDados() 
+	public void preencherDados() {
 		for(int i = 0; i < 10; i++) {
 			String s = String.valueOf(i);
 			usuario[i] = new Usuario("Usuario".concat(s), "dataNascimento".concat(s), "email".concat(s), 
