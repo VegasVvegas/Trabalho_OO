8baa298dba6f281ff9f4b4c05276149e25979e8e
Ana Victória <130088519+navicg@users.noreply.github.com>
2023-11-22 23:54:35 -0300

    Update Artista.java

--- src/entities/Artista.java
+++ src/entities/Artista.java
@@ -1,12 +1,45 @@
 package entities;
-
+import java.util.ArrayList;
+import java.util.Date;
 public class Artista extends Pessoa {
-	private Arraylist albuns;
-	private Arraylist musicas;
-	private int seguidores;
+	private String albuns;
+	private String musicas;
+	private String seguidores;
 	private String sobre;
 	
+	
+public Artista(String nome,
+	   	       String dataNascimento,
+	           String email,
+	           String senha,
+	           String albuns,
+	           String musicas,
+	           String seguidores,
+	           String sobre) {
+		super(nome, dataNascimento, email, senha);
+		this.albuns = albuns;
+		this.musicas = musicas;
+		this.seguidores = seguidores;
+		this.sobre = sobre;
+
+	}
+
+public String getSeguidores() {
+	return seguidores;
+}
+public void setSeguidores(String seguidores) {
+	this.seguidores = seguidores;
+}
+public String getSobre() {
+	return sobre;
+}
+public void setSobre(String sobre) {
+	this.sobre = sobre;
+}
+
+
 	public void adicionarAlbum(String tituloAlbum, String tituloMusica, String genero) {	
+	
 	}
 	public void adicionarMusica(String tituloAlbum, String tituloMusica, String genero) {	
 	}
