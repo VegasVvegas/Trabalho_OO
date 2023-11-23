709ecc18474124d5eae748b2945a43b295c2905a
Matheus-0217 <149620662+Matheus-0217@users.noreply.github.com>
2023-11-22 21:32:32 -0300

    Update Spotify.java

--- src/entities/Spotify.java
+++ src/entities/Spotify.java
@@ -61,7 +61,7 @@ public class Spotify {
 package Spotify;
 
 public class Spotify {
-	private Usuario[] usuarios;
+	private Usuario[] usuario;
 	private int nUsuarios = 0;
 	private Artista[] artistas ;
 	private int nArtistas = 0;
@@ -70,10 +70,10 @@ public class Spotify {
 	private Playlist[] playlist;
 	private int nPlaylist = 0;
 	private Album [] albuns;
-	private int nalbuns = 0;
+	private int nAlbuns = 0;
 	
 	public Spotify() {
-		usuarios = new Usuario[100];
+		usuario = new Usuario[100];
 		artistas = new Artista[50];
 		musicas = new Musica[50];
 		playlist = new Playlist[50];
@@ -82,28 +82,28 @@ public class Spotify {
 	}
 	
 	
-	public Usuario[] getusuarios() {
-		return usuarios;
+	public Usuario[] getUsuarios() {
+		return usuario;
 	}
 	
-	public Usuario getAluno(int i) {
-		return usuarios[i];
+	public Usuario getnUsuario(int i) {
+		return usuario[i];
 	}
 
 	public String[] getNomeUsuarios() {
 		String[] s = new String[nUsuarios];
 		for(int i = 0; i < nUsuarios; i++) {
-			s[i] = usuarios[i].getNome();
+			s[i] = usuario[i].getNome();
 		}
 		return s;
 	}
 	
-	public void setUsuarios(Usuario[] usuarios) {
-		this.usuarios = usuarios;
+	public void setUsuarios(Usuario[] usuario) {
+		this.usuario = usuario;
 	}
 	
-	public void setUsuario(int i, Usuario a) {
-		usuarios[i] = a;
+	public void setUsuario(int i, Usuario r) {
+		usuario[i] = r;
 	}
 
 	public int getnUsuarios() {
@@ -135,8 +135,8 @@ public class Spotify {
 		this.artistas = artistas;
 	}
 	
-	public void setnArtistas(int i, Artista p) {
-		artistas[i] = p;
+	public void setnArtistas(int i, Artista a) {
+		artistas[i] = a;
 	}
 
 	public int getnArtistas() {
@@ -156,13 +156,20 @@ public class Spotify {
 	public Musica getMusica(int i) {
 		return musicas[i];
 	}
+	public String[] getMusica() {
+		String[] s = new String[nMusicas];
+		for(int i = 0; i < nMusicas; i++) {
+			s[i] = musicas[i].getNome();
+		}
+		return s;
+	}
 
 	public void setMusica(Musica[] musicas) {
 		this.musicas = musicas;
 	}
 	
-	public void setMusica(int i, Musica c) {
-		musicas[i] = c;
+	public void setMusica(int i, Musica u) {
+		musicas[i] = u;
 	}
 	
 	public int getnMusica() {
@@ -180,15 +187,29 @@ public class Spotify {
 	public Playlist getPlaylist(int i) {
 		return playlist[i];
 	}
+	public String[] getNomeUsuarios() {
+		String[] s = new String[nUsuarios];
+		for(int i = 0; i < nUsuarios; i++) {
+			s[i] = usuario[i].getNome();
+		}
+		return s;
 
 	public void setPlaylist(Playlist[] playlist) {
 		this.playlist = playlist;
 	}
 	
-	public void setPlaylist(int i, Playlist m) {
-		playlist[i] = m;
+	public void setPlaylist(int i, Playlist p) {
+		playlist[i] = p;
 	}
 
+
+	public String[] getPlaylist() {
+		String[] s = new String[nPlaylist];
+		for(int i = 0; i < nPlaylist; i++) {
+			s[i] = playlist[i].getNome();
+		}
+		return s;
+	}
 
 
 	public int getnPlaylist() {
@@ -201,17 +222,49 @@ public class Spotify {
 		this.nPlaylist = nPlaylist;
 	}
 
+	public Usuario[] getAlbuns() {
+		return albuns;
+	}
+	
+	public Usuario getnAlbuns(int i) {
+		return albuns[i];
+	}
+
+	public String[] getAlbuns() {
+		String[] s = new String[nAlbuns];
+		for(int i = 0; i < nAlbuns; i++) {
+			s[i] = albuns[i].getNome();
+		}
+		return s;
+	}
+	
+	public void setAlbuns(Usuario[] albuns) {
+		this.albuns = albuns;
+	}
+	
+	public void setAlbuns(int i, Album o) {
+		albuns[i] = o;
+	}
+
+	public int getnUsuarios() {
+		return nAlbuns;
+	}
+
+	public void setnUsuarios(int nAlbuns) {
+		this.nAlbuns = nAlbuns;
+	}
 
 
-	public void preencherDados() {
+{
+	public void preencherDados() 
 		for(int i = 0; i < 10; i++) {
 			String s = String.valueOf(i);
-			usuarios[i] = new Usuario("Usuario".concat(s), "dataNascimento".concat(s), "email".concat(s), 
+			usuario[i] = new Usuario("Usuario".concat(s), "dataNascimento".concat(s), "email".concat(s), 
 					"senha".concat(s), "playlistSalvas".concat(s), "albunsFavoritados".concat(s));
 			artistas[i] = new Artista("Artista".concat(s), "dataNascimento".concat(s), "email".concat(s), 
 					"senha".concat(s),"albuns".concat(s), "musicas".concat(s), "seguidores".concat(s),"sob".concat(s));
 			musicas[i] = new Musica("Musica".concat(s), "artista".concat(s),"duracao".concat(s), "album".concat(s));
-			musicas[i].adicionarMusica(new Playlist(usuarios[i], "hoje", i, "turma".concat(s) ));			
+			musicas[i].adicionarMusica(new Playlist(usuario[i], "hoje", i, "turma".concat(s) ));			
 		}
 		nUsuarios = nArtistas = nMusicas = 10;
 	}
